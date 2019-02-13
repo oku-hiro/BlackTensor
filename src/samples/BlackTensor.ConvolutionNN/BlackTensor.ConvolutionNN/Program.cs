6a205f164abc231363887f165f33d7f6ea013856
oku-hiro <oku-hiro@ile.osaka-u.ac.jp>
2019-02-13 10:56:19 +0900

    データ入力の方法をMNISTファイル処理に変更

--- src/samples/BlackTensor.ConvolutionNN/BlackTensor.ConvolutionNN/Program.cs
+++ src/samples/BlackTensor.ConvolutionNN/BlackTensor.ConvolutionNN/Program.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BlackTensor.ConvolutionNN
 {
@@ -11,61 +12,102 @@ namespace BlackTensor.ConvolutionNN
     {
         static void Main(string[] args)
         {
-            double[][][] image_data = new double[10][][];
-            for (int j = 0; j < 10; j++)
+            string image_file = ".\\train-images.idx3-ubyte";
+            string label_file = ".\\train-labels.idx1-ubyte";
+
+            FileStream fs1 = new FileStream(image_file, FileMode.Open);
+            BinaryReader byte_image = new BinaryReader(fs1);
+
+            FileStream fs2 = new FileStream(label_file, FileMode.Open);
+            BinaryReader byte_label = new BinaryReader(fs2);
+
+            byte[][] byte_data1 = new byte[60000][];
+            byte[][] byte_data2 = new byte[60000][];
+            byte[] label_data1 = new byte[60000];
+            byte[] label_data2 = new byte[60000];
+
+            for (int i = 0; i < 60000; i++)
             {
-                image_data[j] = new double[600][];
-                for (int i = 0; i < 600; i++)
+                byte_data1[i] = new byte[784];
+                byte_data2[i] = new byte[784];
+            }
+
+            for (int j = 0; j < 60000; j++)
+            {
+                label_data1[j] = byte_label.ReadByte();
+                for (int i = 0; i < 784; i++)
                 {
-                    image_data[j][i] = new double[784];
+                    byte_data1[j][i] = byte_image.ReadByte();
                 }
             }
 
-            //.\\MNIST\\number0\\image100.bmp　
-            //データの取得先は、http://yann.lecun.com/exdb/mnist/
-            //をbitmapに変えて数字ごとのフォルダに格納しました。
-
-            Bitmap map;
-            for (int k = 0; k < 10; k++)
+            for (int k = 0; k < 60000 - 8; k++)
             {
-                for (int j = 0; j < 600; j++)
-                {
-                    string file_name = ".\\MNIST\\number" + k.ToString() + "\\image" + j.ToString() + ".bmp";
-                    map = new Bitmap(file_name);
+                label_data2[k] = label_data1[k + 8];
 
-                    int n = 0;
-                    for (int y = 0; y < 28; y++)
+                for (int j = 0; j < 28; j++)
+                {
+                    for (int i = 0; i < 14; i++)
                     {
-                        for (int x = 0; x < 28; x++)
-                        {
-                            image_data[k][j][n] = map.GetPixel(x, y).R;
-                            n += 1;
-                        }
+                        int p = i + j * 28;
+                        int q = (i + 14) + j * 28;
+                        byte_data2[k][q] = byte_data1[k][p];
+                    }
+
+                    for (int i = 14; i < 28; i++)
+                    {
+                        int p = i + j * 28;
+                        int q = (i - 14) + j * 28;
+                        byte_data2[k][q] = byte_data1[k][p];
                     }
                 }
+            }
+
+            double[][][] image_data = new double[10][][];
+            for (int k = 0; k < 10; k++)
+            {
+                image_data[k] = new double[500][];
+                for (int j = 0; j < 500; j++)
+                {
+                    image_data[k][j] = new double[784];
+                }
+            }
+
+            int[] step = new int[10];
+            for (int k = 0; k < 6000; k++)
+            {
+                int s = label_data2[k];
+                if (step[s] < 500)
+                {
+                    for (int i = 0; i < 784; i++)
+                    {
+                        image_data[s][step[s]][i] = byte_data2[k][i];
+                    }
+                }
+                step[s] += 1;
             }
 
             LearningParameter lp;
             lp.batch_sample = 10;
             lp.dense_unit = 0;
-            lp.epochs = 375000;
-            lp.data_sample = 6000;
+            lp.epochs = 100000;
+            lp.data_sample = 5000;
             lp.input_channel = 1;
             lp.input_x = 28;
             lp.input_y = 28;
             lp.optimizer = 2;
             lp.lr = Math.Pow(10.0, -4.0);
 
-            lp.output_data = new double[6000][];
-            lp.input_data = new double[6000][];
-            for (int i = 0; i < 6000; i++)
+            lp.output_data = new double[5000][];
+            lp.input_data = new double[5000][];
+            for (int i = 0; i < 5000; i++)
             {
                 lp.output_data[i] = new double[10];
                 lp.input_data[i] = new double[784];
             }
 
             int m = 0;
-            for (int k = 0; k < 600; k++)
+            for (int k = 0; k < 500; k++)
             {
                 for (int j = 0; j < 10; j++)
                 {
